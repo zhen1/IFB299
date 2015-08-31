cfe6e2fb0ef654567e8d854cebc531f37e81895e
Jai <jaispicer@gmail.com>
2015-08-31 15:32:34 +1000

    Login Page
    Remove php from members page started login.php testing to see if
    everything is work

--- login.php
+++ login.php
@@ -1,17 +1,27 @@
 <?php
-session_start();
- 
+
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
 $dbname = "users"; // the name of the database that you are going to use for this project
 $dbuser = "root"; // the username that you created, or were given, to access your database
 $dbpass = "team5"; // the password that you created, or were given, to access your database
- 
-mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
-mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
+
+$dbhandle = mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 
 $myusername = $_POST['user'];
 $mypassword = $_POST['pass'];
 
 $myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
+$mypassword = stripslashes($mypassword); 
+
+$query = "SELECT * FROM 'dbname' WHERE Username='$myusername' and Password='$mypassword'";
+$result = mysql_query($query);
+$count = mysql_num_rows($result);
+
+if($count==1)
+{
+	echo 'It worked';
+}
+
 ?>
+
+mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
