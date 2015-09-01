451a292abe004f91df041299286cc91bce4b5dd5
Jai <jaispicer@gmail.com>
2015-09-01 22:28:17 +1000

    signup form complete
    added all of the sections to the signup form and connected them to
    database

--- new_user.php
+++ new_user.php
@@ -8,6 +8,24 @@
         $dbhandle = mysql_connect($hostname, $username, $password) or die("Could not connect to database");
         $selecttable = mysql_select_db($database, $dbhandle);
 		
+		
+		
+		
+		if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['email']) && isset($_POST['address']))
+		{
+			$fname = $_POST['fname'];
+			$lname = $_POST['lname'];
+			$user = $_POST['user'];
+			$pass = $_POST['pass'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			$address = $_POST['address'];
+		
+			mysql_query("INSERT INTO login (first name, last name, username, password, email, phone number, address) VALUES ('$fname', '$lname', '$user', '$pass', '$email', '$phone', '$address')"); 
+			echo("User created successfully");
+		}
+		
+		
 		mysql_close();
 ?>
 
@@ -21,7 +39,7 @@
 				<p>Password:</p><input type="password" name="pass" />
 				<p>Email:</p><input type="text" name="email" />
 				<p>Phone Number:</p><input type="text" name="phone" />
-				<p>Address:/p><input type="text" name="address" />
+				<p>Address:</p><input type="text" name="address" />
 				<br />
 				<input type="submit" value="Signup" />
 			</form>
