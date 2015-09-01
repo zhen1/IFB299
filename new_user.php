c6ba47684da1d552292cca2aa08db262eb8a2615
Jai <jaispicer@gmail.com>
2015-09-01 22:09:59 +1000

    new_user form complete

--- new_user.php
+++ new_user.php
@@ -10,3 +10,20 @@
 		
 		mysql_close();
 ?>
+
+<html>
+	<body>
+		<h1>Signup</h1>
+			<form action="new_user.php" method="POST">
+				<p>First Name:</p><input type="text" name="fname" />
+				<p>Last Name:</p><input type="text" name="lname" />
+				<p>Username:</p><input type="text" name="user" />
+				<p>Password:</p><input type="password" name="pass" />
+				<p>Email:</p><input type="text" name="email" />
+				<p>Phone Number:</p><input type="text" name="phone" />
+				<p>Address:/p><input type="text" name="address" />
+				<br />
+				<input type="submit" value="Signup" />
+			</form>
+	</body>
+</html>
