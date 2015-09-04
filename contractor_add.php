187e1db1cdf5b6c938d12d6e57e2465e5f2cf4c7
lukekane <luke.kane@connect.qut.edu.au>
2015-09-04 15:08:47 +1000

    change the size of fields on page and maxlength properties

--- contractor_add.php
+++ contractor_add.php
@@ -21,39 +21,44 @@ $success=$_GET['success'];
 	<table style="width: 100%">
 		<tr>
 			<td style="width: 236px">Business Name:</td>
-			<td><input name="businessName" type="text" /></td>
+			<td>
+			<input name="businessName" type="text" maxlength="45" size="45" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Street Number &amp; Street Name:</td>
-			<td><input name="street" type="text" /></td>
+			<td><input name="street" type="text" maxlength="45" size="45" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Suburb:</td>
-			<td><input maxlength="3" name="suburb" type="text" /></td>
+			<td><input maxlength="45" name="suburb" size="45" type="text" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">State:</td>
-			<td><input name="state" type="text" /></td>
+			<td><input maxlength="3" name="state" size="3" type="text" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Postcode:</td>
-			<td><input name="postcode" type="text" /></td>
+			<td><input maxlength="4" name="postcode" size="4" type="text" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Contact Name:</td>
-			<td><input name="contactName" type="text" /></td>
+			<td>
+			<input maxlength="45" name="contactName" size="45" type="text" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Phone Number:</td>
-			<td><input name="phoneNumber" type="text" /></td>
+			<td>
+			<input name="phoneNumber" type="text" maxlength="10" size="10" style="height: 22px" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Email Address:</td>
-			<td><input name="emailAddress" type="text" /></td>
+			<td>
+			<input name="emailAddress" type="text" maxlength="45" size="45" /></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">Notes</td>
-			<td><textarea name="notes" style="width: 419px; height: 105px"></textarea></td>
+			<td>
+			<textarea name="notes" style="width: 419px; height: 105px" cols="45" rows="10"></textarea></td>
 		</tr>
 		<tr>
 			<td style="width: 236px">
