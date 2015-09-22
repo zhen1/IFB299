9514eb093851a96d0f588e5a0b3201da11955d81
Jai <jaispicer@gmail.com>
2015-09-22 12:40:47 +1000

    Fixing the sub menu of the account page

--- templates/account_menu.php
+++ templates/account_menu.php
@@ -6,8 +6,8 @@ $accounttype = $_SESSION['user_type'];
 
 	<div>
 		<ul>
-			<li><a href="volunteers.php">Volunteers</a></li>
-			<li><a href="manager.php">Managers</a></li>
+			<li><a href="account/manager.php">Managers</a></li>
+			<li><a href="account/volunteers.php">Volunteers</a></li>
 			<li><a href="contractor.php">Contractor</a></li>
 			<li><a href="account/details.php">Account Details</a></li>
 			<li><a href="account/job.php">Jobs</a></li>
@@ -17,7 +17,7 @@ $accounttype = $_SESSION['user_type'];
 
 	<div>
 		<ul>
-			<li><a href="volunteers.php">Volunteers</a></li>
+			<li><a href="account/volunteers.php">Volunteers</a></li>
 			<li><a href="contractor.php">Contractor</a></li>
 			<li><a href="account/details.php">Account Details</a></li>
 			<li><a href="account/job.php">Jobs</a></li>
