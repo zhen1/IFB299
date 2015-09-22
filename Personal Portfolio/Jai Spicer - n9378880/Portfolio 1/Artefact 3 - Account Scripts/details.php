bfa7fefe53701c4660f9ea92e357079824f97eba
Jai <jaispicer@gmail.com>
2015-09-22 17:11:26 +1000

    Uploading My Portfolio

---
+++ Personal Portfolio/Jai Spicer - n9378880/Portfolio 1/Artefact 3 - Account Scripts/details.php
@@ -0,0 +1,49 @@
+<?php 
+require("../db_connect.php");
+require("../templates/header_sub.php"); 
+$account = $_SESSION['Username'];
+?>
+
+<link rel="stylesheet" href="../css/style.css">
+<title><?=$account?>'s Account </title>
+
+<?php require("../templates/account_menu_sub.php") ?>
+
+<?php
+	$con = mysql_connect($hostname, $username, $password) or die("Could not connect to database");
+	$database = mysql_select_db($database, $con);
+	$account = $_SESSION['Username'];
+	$table = "logins";
+	$query = "SELECT * FROM $table WHERE Username = '$account'";
+	$result = mysql_query($query);
+	$row = mysql_num_rows($result);
+	$i = 0;
+	
+	while ($i < $row)
+	{
+		$firstname = mysql_result($result, $i, "FirstName"); 
+		$lastname = mysql_result($result, $i, "LastName");
+		$username = mysql_result($result, $i, "Username");
+		$email = mysql_result($result, $i, "email");
+		$phone = mysql_result($result, $i, "PhoneNumber");
+		$address = mysql_result($result, $i, "Address");
+		
+		echo 
+		"
+		<tr><td><b>First Name: </b></td><td>$firstname</td></tr><br/>
+		<tr><td><b>Last Name: </b></td><td>$lastname</td></tr><br/>
+		<tr><td><b>Username: </b></td><td>$username</td></tr><br/>
+		<tr><td><b>Email: </b></td><td>$email</td></tr><br/>
+		<tr><td><b>Phone: </b></td><td>$phone</td></tr><br/>
+		<tr><td><b>Address: </b></td><td>$address</td></tr><br/>
+		";
+		
+		$i++;
+	}
+?>
+
+	<form action="edit.php">
+		<input type="submit" value="Edit Account">
+	</form>
+
+<?php require("../templates/footer.php"); ?>
