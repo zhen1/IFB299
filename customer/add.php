4e570662a684654f6d6a6ec9e3f9e3567d68d04c
lukekane <luke.kane@connect.qut.edu.au>
2015-10-09 14:42:24 +1000

    Started to build the client query pages
    * Have started to build the pages that volunteers use to look up client
    information and to create new client information. Currently incomplete.

---
+++ customer/add.php
@@ -0,0 +1,17 @@
+<!--
+Page for adding new migrant users to the database.
+-->
+
+<?php require("../templates/header_sub.php"); ?>
+
+<link rel="stylesheet" href="../css/style.css">
+<title>Customer Management System - Main Menu</title>
+
+<h1>Customer Management System</h1>		
+<hr>
+<hr>
+<p class="information">Please complete the form to add a new client</p>
+
+
+
+<?php require("../templates/footer.php"); ?>
---
+++ customer/search.php
@@ -0,0 +1,30 @@
+<!--
+page for searching the database for existing migrant users.
+-->
+
+<?php require("../templates/header_sub.php"); ?>
+
+<title>Customer Management System - Main Menu</title>
+
+<h1>Customer Management System</h1>		
+<hr>
+<hr>
+<p class="information">Please complete the form to search for an exisiting 
+client.</p>
+
+
+
+
+<form action="" method="post" autocomplete="off">
+<table>
+<tr><td>Search Terms: </td><td colspan="2">
+	<input name="lastName" type="text" style="width: 197px" /></td></tr>
+<tr><td>Criteria to Seach:	</td><td>Last Name:<input name="Radio1" type="radio" /></td><td>Phone Number:<input name="Radio1" type="radio" /></td><td>Client Number:<input name="Radio1" type="radio" /></td></tr>
+</table>
+</form>
+
+
+
+
+
+<?php require("../templates/footer.php"); ?>
