bddb24cac4e5324bc7b98cbea146b6e08b6e8063
lukekane <luke.kane@connect.qut.edu.au>
2015-10-09 14:45:17 +1000

    Fixed up functionality
    * Made all fields required
    * If a username is already taken and an account is not created, only the
    password will be deleted on refresh. All other data is left for the user
    to modify so they do not have to retype all fields again.

--- signup.php
+++ signup.php
@@ -11,7 +11,18 @@
 		
 		
 		
-		if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['email']) && isset($_POST['address']))
+
+require("templates/header.php"); ?>
+
+<title>Signup</title>
+
+
+	<h1>New Migrant User Signup</h1>
+	<p>Please complete this form to signup for a new account. All fields must be 
+	completed.</p>
+	<?php
+			$validUser = 1;
+			if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['email']) && isset($_POST['address']))
 		{
 			$fname = $_POST['fname'];
 			$lname = $_POST['lname'];
@@ -24,10 +35,12 @@
 			$query = mysql_query("SELECT * FROM $table WHERE Username='$user'");
 			if(mysql_num_rows($query) > 0)
 			{
-				echo 'Username already taken';
+				echo '<p class="unsuccessful">Username already taken. Please try again.</p>';
+				$validUser = 0;
 			}
 			else
 			{
+				$validUser = 1;
 				$password = password_hash($pass, PASSWORD_DEFAULT);
 				mysql_query("INSERT INTO $table (FirstName, LastName, Username, Password, Email, PhoneNumber, Address, UserLevel, Approved) VALUES ('$fname', '$lname', '$user', '$password', '$email', '$phone', '$address', 'Migrant', '1')"); 
 				header("Location:home.php?success=1");
@@ -37,35 +50,28 @@
 		
 ?> 
 
-<?php require("templates/header.php"); ?>
-<title>Signup</title>
-
-
-	<h1>New Migrant User Signup</h1>
-	<p>Please complete this form to signup for a new account.</p>
-	
-	<form action="signup.php" method="POST">
+	<form action="signup.php" method="POST" autocomplete="off">
 		<table>
 		<tr>
-		<td>First Name:</td><td><input type="text" name="fname" /></td>
+		<td>First Name:</td><td><input type="text" name="fname" <?php if ($validUser == 0){ echo 'value="'.$fname.'"'; }?> required/></td>
 		</tr>
 		<tr>		
-		<td>Last Name:</td><td><input type="text" name="lname" /></td>
+		<td>Last Name:</td><td><input type="text" name="lname" <?php if ($validUser == 0){ echo 'value="'.$lname.'"'; }?> required/></td>
 		</tr>
 		<tr>
-		<td>Username:</td><td><input type="text" name="user" /></td>
+		<td>Username:</td><td><input type="text" name="user" <?php if ($validUser == 0){ echo 'value="'.$user.'"'; }?> required/></td>
 		</tr>
 		<tr>
-		<td>Password:</td><td><input type="password" name="pass" /></td>
+		<td>Password:</td><td><input type="password" name="pass" required /></td>
 		</tr>
 		<tr>
-		<td>Email:</td><td><input type="text" name="email" /></td>
+		<td>Email:</td><td><input type="text" name="email" <?php if ($validUser == 0){ echo 'value="'.$email.'"'; }?> required/></td>
 		</tr>
 		<tr>
-		<td>Phone Number:</td><td><input type="text" name="phone" /></td>
+		<td>Phone Number:</td><td><input type="text" name="phone" <?php if ($validUser == 0){ echo 'value="'.$phone.'"'; }?> required/></td>
 		</tr>
 		<tr>
-		<td>Address:</td><td><input type="text" name="address" /></td>
+		<td>Address:</td><td><input type="text" name="address" <?php if ($validUser == 0){ echo 'value="'.$address.'"'; }?> required/></td>
 		</tr>
 		<tr>
 		
